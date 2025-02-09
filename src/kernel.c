90fcd6afa36e81ac149a7846ae3f025aa7666314
apintilie12 <apintilie12@gmail.com>
2025-02-09 10:37:05 +0200

    Removed PL_Uart calls

--- src/kernel.c
+++ src/kernel.c
@@ -1,18 +1,12 @@
 #include "mini_uart.h"
-#include "pl_uart.h"
 
 void kernel_main(void) {
-    // mini_uart_init();
-    // mini_uart_set_baudrate(9600);
-    // mini_uart_send_string("Sending at 9600 baudrate!\r\n");
+    mini_uart_init();
+    mini_uart_set_baudrate(115200);
+    mini_uart_send_string("Sending at 9600 baudrate!\r\n");
 
-    // while(1) {
-    //     mini_uart_send(mini_uart_recv());
-    // }
-    pl_uart_init();
-    pl_uart_send_string("Hello from the main UART!\r\n");
-
-    while (1) {
-        pl_uart_send(pl_uart_recv());
+    while(1) {
+        mini_uart_send(mini_uart_recv());
     }
+    
 }

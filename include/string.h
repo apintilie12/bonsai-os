a5772772f4072206af125084bebc14c4037d0fcd
apintilie12 <apintilie12@gmail.com>
2025-02-09 14:44:41 +0200

    Added some string utils and test target in makefile

---
+++ include/string.h
@@ -0,0 +1,9 @@
+#ifndef _STRING_H
+#define _STRING_H
+
+void itoa(unsigned long val, char *buf);
+
+void strcat(char *destination, const char *source);
+void strrev(char *source);
+
+#endif /*_STRING_H*/
---
+++ src/string.c
@@ -0,0 +1,46 @@
+#include "string.h"
+
+void itoa (unsigned long val, char *buf) {
+    char d;
+    char *p = buf;
+    while (val > 0)
+    {
+       d = val % 10;
+       val /= 10;
+       d += '0';
+       *p = d;
+       p++;
+    }
+    *p = 0;
+    strrev(buf);
+}
+
+void strrev(char *source) {
+    char *tail = source;
+    while (*tail != 0) {
+        tail++;
+    }
+    tail--;
+    char *head = source;
+    while(head < tail) {
+        char aux = *tail;
+        *tail = *head;
+        *head = aux;
+        tail--;
+        head++;
+    }
+}
+
+void strcat(char *destination, const char *source) {
+    char *p_dest = destination;
+    char *p_source = source;
+    while(*p_dest != 0) {
+        p_dest++;
+    }
+    while (*p_source != 0) {
+        *p_dest = *p_source;
+        p_dest++;
+        p_source++;
+    }
+    *p_dest = 0;
+}

b65d5009d99847865c2b879eb3fa52d277951db5
apintilie12 <apintilie12@gmail.com>
2025-02-25 15:00:23 +0200

    Added basic exception level setting up

--- src/kernel.c
+++ src/kernel.c
@@ -1,15 +1,13 @@
 #include "mini_uart.h"
 #include "printf.h"
+#include "utils.h"
 
 void kernel_main(void) {
     mini_uart_init();
     init_printf(0, putc);
     mini_uart_set_baudrate(9600);
-    mini_uart_send_string("Sending at 9600 baudrate!\r\n");
-    for(int i = 0; i < 10; i++) {
-        printf("I at value: %d\r\n", i);
-    }
-
+    int el = get_el();
+    printf("Exception level: %d \r\n", el);
     while(1) {
         mini_uart_send(mini_uart_recv());
     }

1da954b483b35eec328cac1601669da91a81438f
apintilie12 <apintilie12@gmail.com>
2025-02-26 11:21:59 +0200

    Initial attempt to switch to EL2

--- src/kernel.c
+++ src/kernel.c
@@ -2,12 +2,19 @@
 #include "printf.h"
 #include "utils.h"
 
-void kernel_main(void) {
+void kernel_init(void) {
     mini_uart_init();
     init_printf(0, putc);
     mini_uart_set_baudrate(9600);
+}
+
+void print_el(void) {
     int el = get_el();
-    printf("Exception level: %d \r\n", el);
+    printf("Exception level: %d\r\n", el);
+}
+
+void kernel_main(void) {
+    print_el();
     while(1) {
         mini_uart_send(mini_uart_recv());
     }

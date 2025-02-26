0ab1a98192628c60ae1038f56568d09a82593abc
apintilie12 <apintilie12@gmail.com>
2025-02-26 12:32:27 +0200

    Added basic interrupt support

--- src/kernel.c
+++ src/kernel.c
@@ -1,13 +1,17 @@
 #include "mini_uart.h"
 #include "printf.h"
-#include "utils.h"
+#include "irq.h"
+#include "timer.h"
 
 void kernel_main(void) {
     mini_uart_init();
     init_printf(0, putc);
     mini_uart_set_baudrate(9600);
-    int el = get_el();
-    printf("Exception level: %d \r\n", el);
+    irq_vector_init();
+    timer_init();
+    enable_interrupt_controller();
+    enable_irq();
+    
     while(1) {
         mini_uart_send(mini_uart_recv());
     }
